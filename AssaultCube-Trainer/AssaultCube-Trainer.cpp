c83607cf928bf37a7345220e8e7df9a9c1705a81
karistin <seongjung1109@gmail.com>
2023-06-19 22:58:20 +0900

    feat : make game cheat

--- AssaultCube-Trainer/AssaultCube-Trainer.cpp
+++ AssaultCube-Trainer/AssaultCube-Trainer.cpp
@@ -1,51 +1,157 @@
 ﻿
 #include "stdafx.h"
 #include "proc.h"
+#include "mem.h"
 #include <iostream>
 
-
+//https://learn.microsoft.com/ko-kr/windows/win32/inputdev/virtual-key-codes
+// VK key set 
 int main()
 {
-    // get proID of target process
-    DWORD procId = GetProcId(L"ac_client.exe");
-    //std::cout << "procId = " << std::dec << procId << std::endl;
+	const wchar_t gameName[] = L"ac_client.exe";
+	
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    //get module base address
-    uintptr_t moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
-    //std::cout << "moduleBase = " << "0x" << std::hex << moduleBase << std::endl;
+	HANDLE hProcess = 0;
 
-    //get handle to process 
-    HANDLE hProcess = 0;
-    hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
-    //https://learn.microsoft.com/ko-kr/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
-    
-   
-   //Resolve base address of the pointer chain 
-    uintptr_t dynamicPtrBaseAddr = moduleBase + 0x18AC00;
+	uintptr_t moduleBase = 0, localPlayerPtr = 0, healthAddr = 0, ammoAddr = 0, recoil1 = 0, recoil2 = 0;
+	bool bHealth = false, bAmmo = false, bRecoil = false; 
 
-    std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicPtrBaseAddr << std::endl;
-    
-    //Resolve our ammo pointer chain 
-    //std::vector<unsigned int> ammoOffsets = {0x0140, 0x012C, 0x0144};
-    uintptr_t ammoAddr = FindMAAddy(hProcess, dynamicPtrBaseAddr, {0x0140});
-    std::cout << "ammoAddr = " << "0x" << std::hex << ammoAddr << std::endl;
+	const int newValue = 9999;
+	// hp value
 
-    //Read Ammo value
-    int ammoValue = 0;
-    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
-    std::cout << "Current ammo = " << std::dec << ammoValue << std::endl;
+	const int fly_on = 5;
+	// 5 : fly , 0 : default
+	const int fly_off = 0;
 
-    //Write to it
-    int newAmmo = 1337;
-    WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
-
-    //Read out again
-    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
-    std::cout << "New ammo = " << std::dec << ammoValue << std::endl;
+	const int no_recoil_on = 0;
+	const int recoil_1 = 25;
+	const int recoil_2 = 50;
 
 
-    getchar();
-    return 0;
+	DWORD procId = GetProcId(gameName);
+
+	if (procId)
+	{
+		hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+
+		moduleBase = GetModuleBaseAddress(procId, gameName);
+
+		localPlayerPtr = moduleBase + 0x17E0A8;
+
+		healthAddr = FindMAAddy(hProcess, localPlayerPtr, {0xEC});
+
+		//ammoAddr = FindMAAddy(hProcess, localPlayerPtr, {0x367 + 0x14});
+
+		recoil1 = FindMAAddy(hProcess, localPlayerPtr, { 0x364, 0xC, 0x5E });
+		recoil2 = FindMAAddy(hProcess, localPlayerPtr, { 0x364, 0xC, 0x60 });
+
+
+		SetConsoleTextAttribute(hConsole, 2);
+		std::cout << "ac.client.exe process FOUND! \r\nStaring Menu ..... \r\n ----------------"<<std::endl;
+
+	}
+	else
+	{
+		SetConsoleTextAttribute(hConsole, 4);
+		std::cout << "Process not found, press enter to exit " << std::endl;
+		getchar();
+		return 0;
+	}
+
+	DWORD dwExit = 0;
+
+	std::string health_str = "[Z] HEALTH HACK \n",
+		ammo_str = "[C] INFINITE AMMO \n",
+		recoil_str = "[X] NO RECOIL \n",
+		fly_str = "[NUMPAD 1] FLY HACK \n--------------------------\n";
+
+	std::cout << health_str << ammo_str << recoil_str << fly_str;
+
+	while (GetExitCodeProcess(hProcess, &dwExit) && dwExit == STILL_ACTIVE)
+	{
+		
+		(bHealth) ? health_str += "[ENABLED] | " : health_str;
+		(bAmmo) ? ammo_str += "[ENABLED] | " : ammo_str;
+		(bRecoil) ? recoil_str += "[ENABLED] | " : recoil_str;
+
+		// press Z
+		if (GetAsyncKeyState(0x5A) & 1)
+		{
+			bHealth = !bHealth;
+			if (bHealth)
+			{
+				mem::PatchEx((BYTE*)healthAddr, (BYTE*)&newValue, sizeof(newValue), hProcess);
+				SetConsoleTextAttribute(hConsole, 2);
+				std::cout << "Health Activated ... \r\n";
+			}
+			else
+			{
+				SetConsoleTextAttribute(hConsole, 4);
+				std::cout << "Health Deactivated .. \r\n";
+			}
+			
+		}
+		// press c
+		if (GetAsyncKeyState(0x43) & 1)
+		{
+			bAmmo = !bAmmo;
+			if (bAmmo)
+			{
+				mem::PatchEx((BYTE*)(moduleBase + 0xc73EF), (BYTE*)"\xFF\x00", 2, hProcess); // FF 06 - inc [esi]
+				SetConsoleTextAttribute(hConsole, 2);
+
+				std::cout << "Infinite Ammo Activated...\n";
+
+			}
+			else
+			{
+				mem::PatchEx((BYTE*)(moduleBase + 0xc73EF), (BYTE*)"\xFF\x08", 2, hProcess); // FF 0E - dec [esi]
+				SetConsoleTextAttribute(hConsole, 4);
+
+				std::cout << "Infinite Ammo Deactivated...\n";
+
+			}
+
+		}
+
+		// press x
+		if (GetAsyncKeyState(0x58) & 1) // Key X - No Recoil
+		{
+			bRecoil = !bRecoil;
+
+			if (bRecoil)
+			{
+				mem::PatchEx((BYTE*)(recoil1), (BYTE*) &no_recoil_on, sizeof(no_recoil_on), hProcess);
+				mem::PatchEx((BYTE*)(recoil2), (BYTE*) &no_recoil_on, sizeof(no_recoil_on), hProcess);
+				SetConsoleTextAttribute(hConsole, 2);
+
+				std::cout << "No Recoil Activated...\n";
+
+			}
+			else
+			{
+
+				mem::PatchEx((BYTE*)(recoil1), (BYTE*)&recoil_1, sizeof(recoil_1), hProcess);
+				mem::PatchEx((BYTE*)(recoil2), (BYTE*)&recoil_2, sizeof(recoil_2), hProcess);
+				SetConsoleTextAttribute(hConsole, 4);
+				std::cout << "No Recoil Deactivated...\n";
+
+			}
+		}
+
+		if (GetAsyncKeyState(VK_INSERT) & 1)
+		{
+			return 0;
+		}
+
+		Sleep(25);
+		
+	}
+	CloseHandle(hProcess);
+
+	return 0;
 }
+
 
 
---
+++ AssaultCube-Trainer/mem.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <windows.h>
+#include <vector>
+#include <TlHelp32.h>
+
+namespace mem
+{
+	void PatchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess);
+	void NopEx(BYTE* dst, unsigned int size, HANDLE hProcess);
+}
